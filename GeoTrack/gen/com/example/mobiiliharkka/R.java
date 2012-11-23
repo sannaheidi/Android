89728d53f8b6e49795b5cc9c038da7ce37ab1983
Otto Hytönen <otsahyto@student.jyu.fi>
2012-11-23 22:29:32 +0200

    T�ss� minuu

--- GeoTrack/gen/com/example/mobiiliharkka/R.java
+++ GeoTrack/gen/com/example/mobiiliharkka/R.java
@@ -245,6 +245,7 @@ containing a value of this type.
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int gpslist=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
